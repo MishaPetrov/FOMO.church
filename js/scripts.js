b2851ee1b117534e7d5b2cf17b1b5df505cef779
Kayla Ondracek and Misha Petrov <kayla@>
2018-01-31 11:49:44 -0800

    retrieve start date from API and set min date in calendar. still need to convert value set in min date to ISO value

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,15 @@ var SDK = typeof window !== 'undefined' ? window.COIN_API_SDK : require("./coina
 var cryptoApi = new SDK("7F2E3F11-2194-4945-9888-16822C39CCD0"); //"2014-11-02T23:59:59"
 //var numberOfCoins;
 
-function SortCoin(name, id, imgUrl) {
+function SortCoin(name, id, sortId, imgUrl) {
   this.name = name;
   this.id = id;
+  this.sortId = sortId;
   this.imgUrl = imgUrl;
+}
+
+function convertDate(date) {
+  return date;
 }
 
 // function calculate(coinType, fiatAmount, fiatType, startDate, endDate) {
@@ -30,26 +35,29 @@ $(document).ready(function() {
   $.get("https://min-api.cryptocompare.com/data/all/coinlist", function(response) {
     var listItems = '';
     var sortedCoins = [];
+    console.log(response);
+
     //var listItems = '<option selected="selected" value="0">- Select -</option>';
     Object.keys(response.Data).forEach(function(key) {
       var coinName = key;
       var sortId = response.Data[key].SortOrder;
       var imgUrl = response.Data[key].ImageUrl;
-      var sortedCoin = new SortCoin(coinName, sortId, imgUrl);
+      var id = response.Data[key].Id;
+      var sortedCoin = new SortCoin(coinName, id, sortId, imgUrl);
       sortedCoins.push(sortedCoin);
     });
 
-    sortedCoins.sort(function(a, b){
+    sortedCoins.sort(function(a, b) {
       return a.id-b.id;
     });
 
     sortedCoins.forEach(function(sortedCoin) {
-      listItems += "<li value='" + sortedCoin.name + "'><a href='#'><img class='crypto-icon' src='https://www.cryptocompare.com" + sortedCoin.imgUrl + "'><span class='key-span'>" + sortedCoin.name + "</span></a></li>";
+      listItems += "<li value='" + sortedCoin.id + "'><a href='#'><img class='crypto-icon' src='https://www.cryptocompare.com" + sortedCoin.imgUrl + "'><span class='key-span'>" + sortedCoin.name + "</span></a></li>";
     });
     //$("#coinType").append('<input class="form-control" id="coin-type-input" type="text" placeholder="Search..">');
     $("#coinType").append(listItems);
     $("#coin-type-input").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
+      var value = $(this).text().toLowerCase();
       $("#coinType li").filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
       });
@@ -57,8 +65,14 @@ $(document).ready(function() {
     $("li").click(function() {
       coinTest = $(this).text();
       $(".crypto-search-btn").text(coinTest);
-    });
 
+      var requestStartDate = "https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?id=" + $(this).val();
+      $.get(requestStartDate, function(response) {
+        // WE ARE HERE
+        var date = response.Data.General.StartDate;
+        $("#buy-date").attr("min", date.toISOString());
+      });
+    });
   });
 
   $("button#calculate").click(function(event) {
@@ -106,5 +120,5 @@ $(document).ready(function() {
 
 
 
-  })
+  });
 });
